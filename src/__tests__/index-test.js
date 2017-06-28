9f47aab9ba2f64ef4ff7bba4626ff71d4f213bc6
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2017-06-28 18:35:36 +0300

    Chore(package): update deps.

--- src/__tests__/index-test.js
+++ src/__tests__/index-test.js
@@ -1,20 +1,20 @@
-import path from 'path';
-import test from 'ava'; // eslint-disable-line node/no-unpublished-import
-import wpCli from '../index';
+import path from "path";
+import test from "ava"; // eslint-disable-line node/no-unpublished-import
+import wpCli from "../index";
 
-const fixturesDir = path.resolve(__dirname, 'fixtures');
+const fixturesDir = path.resolve(__dirname, "fixtures");
 
-test('should execute successfully', async t => {
-    const result = await wpCli(path.join(fixturesDir, './wp-cli.phar'));
+test("should execute successfully", async t => {
+    const result = await wpCli(path.join(fixturesDir, "./wp-cli.phar"));
 
     t.true(result.code === 0);
     t.regex(result.stdout, /WP-CLI/);
     t.true(result.stderr.length === 0);
 });
 
-test('should execute custom args successfully', async t => {
-    const result = await wpCli(path.join(fixturesDir, './wp-cli.phar'), [
-        '--help'
+test("should execute custom args successfully", async t => {
+    const result = await wpCli(path.join(fixturesDir, "./wp-cli.phar"), [
+        "--help"
     ]);
 
     t.true(result.code === 0);
@@ -22,8 +22,8 @@ test('should execute custom args successfully', async t => {
     t.true(result.stderr.length === 0);
 });
 
-test('should execute custom args successfully and options', async t => {
-    const result = await wpCli('./wp-cli.phar', ['--help'], {
+test("should execute custom args successfully and options", async t => {
+    const result = await wpCli("./wp-cli.phar", ["--help"], {
         cwd: fixturesDir
     });
 
@@ -32,15 +32,15 @@ test('should execute custom args successfully and options', async t => {
     t.true(result.stderr.length === 0);
 });
 
-test('should execute failed if not found bin', t => t.throws(wpCli()));
+test("should execute failed if not found bin", t => t.throws(wpCli()));
 
-test('should execute failed with unknown command', t =>
-    t.throws(wpCli(path.join(fixturesDir, './wp-cli.phar'), ['unknown'])));
+test("should execute failed with unknown command", t =>
+    t.throws(wpCli(path.join(fixturesDir, "./wp-cli.phar"), ["unknown"])));
 
-test('should execute failed and catching', t =>
+test("should execute failed and catching", t =>
     t.throws(
-        wpCli(path.join(fixturesDir, './wp-cli.phar'), [
-            'unknown'
+        wpCli(path.join(fixturesDir, "./wp-cli.phar"), [
+            "unknown"
         ]).catch(error => {
             t.true(error.code === 1);
             t.true(error.stdout.length === 0);
